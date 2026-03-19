c95bce9b21298d5728379e4bb2b7e65395e4d974
5ceansdeep <su5653@naver.com>
2026-03-19 11:00:52 +0900

    [REFACTOR] Express->NestJS로 백엔드 프레임워크 전환

---
+++ backend/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('DRIFT API')
+    .setDescription('음악 취향 기반 우주 탐험 플랫폼')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3001);
+  console.log('Server running on http://localhost:3001');
+  console.log('Swagger docs: http://localhost:3001/api-docs');
+}
+
+bootstrap();
